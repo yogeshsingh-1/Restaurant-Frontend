fec222574c1f07f62ae2135ae53de8b02135574f
yogeshsingh-1 <yogesh.singh@mygstcafe.in>
2026-01-06 15:21:46 +0530

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import "./Home.css"
+import { NavLink } from 'react-router-dom'
 const Home = () => {
+  const active = (e) => {
+    e.setAttribute("className", "active")
+  }
   return (
-   <>
-   <div>Home</div>
-   </>
+    <>
+      <div className='home'>
+        <div className="navbar">
+          <h3>Cake<br /><span> & Biscuit</span></h3>
+          <ul>
+            <li><NavLink to="/">WishList</NavLink></li>
+            <li><NavLink to="/explore">Explore</NavLink></li>
+            <li><NavLink to="/service">Service</NavLink></li>
+            <li><NavLink to="/Contact">Contact</NavLink></li>
+          </ul>
+          <div className="right">
+            <p>Proceed</p>
+            <p>Chutney</p>
+            <button>Connect</button>
+          </div>
+        </div>
+        <div className="login">
+          <button>Book Now</button>
+        </div>
+      </div>
+    </>
   )
 }
 
