8230d6b3e7af97c4c840f8fbaf28d25c1af568e0
yogeshsingh-1 <yogesh.singh@mygstcafe.in>
2026-01-06 15:31:58 +0530

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,23 +2,31 @@ import React from 'react'
 import "./Home.css"
 import { NavLink } from 'react-router-dom'
 const Home = () => {
-  const active = (e) => {
-    e.setAttribute("className", "active")
-  }
+
+  const navItems = [
+    { label: "WishList", path: "/" },
+    { label: "Explore", path: "/explore" },
+    { label: "Service", path: "/service" },
+    { label: "Contact", path: "/contact" },
+  ];
+
   return (
     <>
       <div className='home'>
         <div className="navbar">
           <h3>Cake<br /><span> & Biscuit</span></h3>
           <ul>
-            <li><NavLink to="/">WishList</NavLink></li>
+            {/* <li><NavLink to="/">WishList</NavLink></li>
             <li><NavLink to="/explore">Explore</NavLink></li>
             <li><NavLink to="/service">Service</NavLink></li>
-            <li><NavLink to="/Contact">Contact</NavLink></li>
+            <li><NavLink to="/Contact">Contact</NavLink></li> */}
+            {
+              navItems.map((item) => <li><NavLink to={item.path}>{item.label}</NavLink></li>)
+            }
           </ul>
           <div className="right">
-            <p>Proceed</p>
-            <p>Chutney</p>
+            <p><NavLink to="/ch">Proceed</NavLink></p>
+            <p><NavLink to="/ch">Chutney</NavLink></p>
             <button>Connect</button>
           </div>
         </div>
